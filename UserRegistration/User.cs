7531fc91737917df505ff6b61b906341d507918a
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-09 20:44:25 +0530

    [Priya] add . samples

--- UserRegistration/User.cs
+++ UserRegistration/User.cs
@@ -129,43 +129,52 @@ namespace UserRegistration
             }
         }
         public void VerifiedEmailList()
-        { 
-            List<string> emailList = new List<string>();
-            emailList.Add("abc@yahoo.com");
-            emailList.Add("abc-100@yahoo.com");
-            emailList.Add("abc.100@yahoo.com");
-            emailList.Add("abc111@yahoo.com");
-            emailList.Add("abc-100@abc.net");
-            emailList.Add("abc.100@abc.com.au");
-            emailList.Add("abc@1.com");
-            emailList.Add("abc@gmail.com.com");
-            emailList.Add("abc+100@gmail.com");
 
-            emailList.Add("abc");
-            emailList.Add("abc@.com.my");
-            emailList.Add("abc123@gmail.a");
-            emailList.Add("abc123@.com");
-            emailList.Add("abc123@.com.com");
-            emailList.Add(".abc@abc.com");
-            emailList.Add("abc()*@gmail.com");
-            emailList.Add("abc@%*.com");
-            emailList.Add("abc..2002@gmail.com");
-            emailList.Add("abc.@gmail.com");
-            emailList.Add("abc@abc@gmail.com");
-            emailList.Add("abc@gmail.com.1a");
-            emailList.Add("abc@gmail.com.aa.au");
+        {
+
             {
-                Regex regex = new Regex("^[a-z0-9-+]+([.][a-z0-9+-]+)?@[a-z0-9]+[.][a-z]{2,}([.][a-z]{2,})?$");
-            }
-            foreach (string email in emailList)
-            {
-                if (regex.IsMatch(email))
-                    Console.WriteLine(email + "- Valid Email");
-                else
-                    Console.WriteLine(email + "- Invalid Email");
+                List<string> emailList = new List<string>();
+                emailList.Add("abc@yahoo.com");
+                emailList.Add("abc-100@yahoo.com");
+                emailList.Add("abc.100@yahoo.com");
+                emailList.Add("abc111@yahoo.com");
+                emailList.Add("abc-100@abc.net");
+                emailList.Add("abc.100@abc.com.au");
+                emailList.Add("abc@1.com");
+                emailList.Add("abc@gmail.com.com");
+                emailList.Add("abc+100@gmail.com");
+
+                emailList.Add("abc");
+                emailList.Add("abc@.com.my");
+                emailList.Add("abc123@gmail.a");
+                emailList.Add("abc123@.com");
+                emailList.Add("abc123@.com.com");
+                emailList.Add(".abc@abc.com");
+                emailList.Add("abc()*@gmail.com");
+                emailList.Add("abc@%*.com");
+                emailList.Add("abc..2002@gmail.com");
+                emailList.Add("abc.@gmail.com");
+                emailList.Add("abc@abc@gmail.com");
+                emailList.Add("abc@gmail.com.1a");
+                emailList.Add("abc@gmail.com.aa.au");
+                {
+                    Regex regex = new Regex("^[a-z0-9-+]+([.][a-z0-9+-]+)?@[a-z0-9]+[.][a-z]{2,}([.][a-z]{2,})?$");
+                }
+                foreach (string email in emailList)
+                {
+                    if (regex.IsMatch(email))
+                        Console.WriteLine(email + "- Valid Email");
+                    else
+                        Console.WriteLine(email + "- Invalid Email");
+                    foreach (string email in emailList)
+                    {
+                        if (regex.IsMatch(email))
+                            Console.WriteLine(email + "- VALID");
+                        else
+                            Console.WriteLine(email + "- INVALID");
+                    }
+                }
             }
         }
     }
 }
-
-
